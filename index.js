0b54e13142dfac7d690083eff5bb7b30df55a70f
Ho Tan Hung <tanhungho2002@gmail.com>
2024-08-04 12:57:23 +0700

    fix deploy

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
-const path = require('path')
-const express = require("express")
-require('dotenv').config()
-const app = express()
-const cors = require("cors")
-const port = process.env.PORT || 3000
+const path = require("path");
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+require("dotenv").config();
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 3000;
 
 // Page Home
 app.get("/", (req, res) => {
-    res.send('SERVER ON')
-})
+  res.send("SERVER ON");
+});
 
 // ZingMp3Router
-const ZingMp3Router = require("./src/routes/ZingRouter")
-app.use("/api", cors({ origin: '*' }), ZingMp3Router)
-
+const ZingMp3Router = require("./src/routes/ZingRouter");
+app.use("/api", cors({ origin: "*" }), ZingMp3Router);
+app.use(
+  "/api",
+  createProxyMiddleware({
+    target: "https://api-zingmp3-vercel-sigma.vercel.app/", // Thay bằng URL API gốc của bạn
+    changeOrigin: true,
+    pathRewrite: {
+      "^/api": "", // Xóa tiền tố '/api' khỏi URL
+    },
+  })
+);
 // Page Error
 app.get("*", (req, res) => {
-    res.send("Nhập Sai Đường Dẫn! Vui Lòng Nhập Lại >.<")
+  res.send("Nhập Sai Đường Dẫn! Vui Lòng Nhập Lại >.<");
 });
 
 app.listen(port, () => {
-    console.log(`Start server listen at http://localhost:${port}`)
+  console.log(`Start server listen at http://localhost:${port}`);
 });
